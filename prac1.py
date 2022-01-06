46e23c0f2411e4b9da49ea1af4804ff8a9eeedae
arpan-k09 <arpanak1999@gmail.com>
2022-01-06 11:31:10 -0500

    Implemented first task to merege the csv files to single csv file named as 'allsalesdata.csv'.

--- prac1.py
+++ prac1.py
@@ -1,1 +1,19 @@
 '''/Users/arpankorat/PycharmProjects/pandas-prac/Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data/Sales_August_2019.csv'''
+
+import pandas as pd
+import os
+
+# x = pd.read_csv('./Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data/Sales_August_2019.csv')
+
+#
+files = [file for file in os.listdir('./Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data')]
+
+# print(files)
+allfiles = pd.DataFrame()
+for i in files:
+    df = pd.read_csv('./Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data/'+i)
+    print(df.shape)
+    allfiles = pd.concat([allfiles,df])
+
+print(allfiles.shape)
+allfiles.to_csv('allsalesdata.csv',header=True,index=False)
